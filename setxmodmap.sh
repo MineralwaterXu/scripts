701ef5a7f74606cfad5dec71187b22260a510e8c
MineralwaterXu <mineralwater.xu@gmail.com>
2020-12-09 23:14:02 +0800

    get the id of Logitech MX Anywhere 2S and change the mapping

--- setxmodmap.sh
+++ setxmodmap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 setxkbmap us -option -option caps:swapescape
-xinput set-button-map 13 1 2 3 4 5 2 2 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+for id in $(xinput list | grep 'Logitech MX Anywhere 2S' |  grep pointer | cut -d '=' -f 2 | cut -f 1);
+	do xinput --set-button-map $id 1 2 3 4 5 2 2 7 8 9 10 11 12 13 14 15 16 17 18 19 20;
+done
