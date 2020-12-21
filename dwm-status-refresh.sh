d3f1b4a32b7ed1158fcc6c581bbcdeb0fdc3a4a5
MineralwaterXu <mineralwater.xu@gmail.com>
2020-12-21 16:50:29 +0800

    add mail status

--- dwm-status-refresh.sh
+++ dwm-status-refresh.sh
@@ -139,7 +139,7 @@ export IDENTIFIER="unicode"
 #. "$DIR/dwmbar-functions/dwm_cmus.sh"
 #. "$DIR/dwmbar-functions/dwm_resources.sh"
 #. "$DIR/dwmbar-functions/dwm_battery.sh"
-#. "$DIR/dwmbar-functions/dwm_mail.sh"
+. "$DIR/dwmbar-functions/dwm_mail.sh"
 #. "$DIR/dwmbar-functions/dwm_backlight.sh"
 . "$DIR/dwmbar-functions/dwm_alsa.sh"
 #. "$DIR/dwmbar-functions/dwm_pulse.sh"
@@ -156,7 +156,7 @@ get_bytes
 vel_recv=$(get_velocity $received_bytes $old_received_bytes $now)
 vel_trans=$(get_velocity $transmitted_bytes $old_transmitted_bytes $now)
 
-xsetroot -name "  💿 $(print_mem)M $(dwm_alsa) $(print_date) "
+xsetroot -name "  💿 $(print_mem)M $(dwm_mail) $(dwm_alsa) $(print_date) "
 
 # Update old values to perform new calculations
 old_received_bytes=$received_bytes
