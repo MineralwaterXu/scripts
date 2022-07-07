a348df2889545416330504b91b9aa708f65b2eb7
Mineralwater Xu <mineralwater@m1neralwater.com>
2022-07-07 13:39:04 +0800

    add dualmon.sh

--- autostart.sh
+++ autostart.sh
@@ -2,6 +2,7 @@
 
 /bin/bash ~/scripts/dwm-status.sh &
 picom -b
-feh --bg-fill ~/Pictures/Айвазовский_И.К._Волна.jpg
+feh -g 2560x1440 --bg-fill ~/Pictures/Айвазовский_И.К._Волна.jpg  -g 1920x1200 -bg-fill ~/Pictures/Айвазовский_И.К._Волна.jpg
 /bin/bash ~/scripts/setxmodmap.sh &
 /bin/bash ~/scripts/autostart_wait.sh &
+/bin/bash ~/scripts/dualmon.sh &
---
+++ dualmon.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xrandr --auto --output DP-0 --pos 1920x0 --mode 2560x1440 --primary
+xrandr --auto --output DP-4 --pos 0x240 --mode 1920x1200
